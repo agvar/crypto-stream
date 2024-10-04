f1ff310c3984290f63201c8dc974087d0a268853
agvar <ajeebgeorge@gmail.com>
2024-10-04 15:03:45 -0400

    handle request response

--- src/main/data_ingestion/base_service.py
+++ src/main/data_ingestion/base_service.py
@@ -1,16 +1,33 @@
 from utils.base_component import BaseComponent
 import requests
+import os
+from dotenv import load_dotenv
+from typing import Dict
+
 
 class BaseService(BaseComponent):
     def __init__(self,config,section_name):
         super().__init__(config,section_name)
         
     def initialize(self):
-        self.api_endpoint = self.config.get('api_endpoint')
-        self.api_key = self.config.get('api_key')
+        load_dotenv()
+        self.api_key = os.getenv('API_KEY')
+        self.api_endpoint = self.config.get('api_endpoint')    
+   
+    def _request_response(self)-> Dict:
+        try:
+            headers = {'Authorization':f'Bearer{self.api_key}'}
+            response = requests.get(self.api_endpoint,headers=headers)
+            if response.status_code != 200:
+                raise Exception(f'API response error:{response.status_code}')
+                self.logger.exception(response.status_code,exc_info=True)
+            else:
+                return response.json()
+        except Exception as e:
+            self.logger.exception(e,exc_info=True)
+            raise Exception(f'error:{e}')
 
-    def _send_request(self):
-        headers = {'Authorization':f'Bearer{self.api_key}'}
 
     def run(self):
-        pass
+       self.initialize()
+       response = self._request_response()
