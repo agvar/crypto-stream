cf7f02d2a9b974dd3cfd18b4abb654df6e348471
agvar <ajeebgeorge@gmail.com>
2024-10-04 07:58:06 -0400

    creating the base component

---
+++ src/main/utils/base_component.py
@@ -0,0 +1,36 @@
+import logging
+from configparser import ConfigParser
+from abc import ABC,abstractmethod
+from dataclasses import dataclass    
+
+class BaseComponent(ABC):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self,config_file:str,section_name:str):
+        self.setup_logging()
+        self._read_config(config_file,section_name)
+
+    def _setup_logging(self):
+        self.logger.setLevel("INFO")
+        formatter = logging.Formatter(format="%(asctime)s - %(levelname)s- %(message)s",style='%')
+        handler = logging.FileHandler("crypto_stream.log")
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+    
+    def _read_config(self,config_file:str,section_name:str) -> dict :
+        config = ConfigParser()
+        config.read(config_file)
+        try:
+            return dict(config[section_name])
+        except ConfigParser.NoSectionError as error:
+            self.logger.error(error,exc_info=True) 
+
+    @abstractmethod
+    def initialize(self):
+        pass
+    
+    @abstractmethod
+    def run(self):
+        pass
+
+
